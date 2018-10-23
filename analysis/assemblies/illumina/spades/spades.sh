466f0899b4d904aad6e3234e7251acc8e20e0b83
Nick Loman <n.j.loman@bham.ac.uk>
2018-10-23 16:04:00 +0000

    spades assemblies by isolate

---
+++ analysis/assemblies/illumina/spades/spades.sh
@@ -0,0 +1,10 @@
+spades.py -1 in506_1_R1.fastq.gz -2 in506_1_R2.fastq.gz -t 12 -m 512 -o in506_1_Salmonella_enterica
+spades.py -1 in506_2_R1.fastq.gz -2 in506_2_R2.fastq.gz -t 12 -m 512 -o in506_2_Staphylococcus_aureus
+spades.py -1 in506_3_R1.fastq.gz -2 in506_3_R2.fastq.gz -t 12 -m 512 -o in506_3_Enterococcus_faecalis
+spades.py -1 in506_4_R1.fastq.gz -2 in506_4_R2.fastq.gz -t 12 -m 512 -o in506_4_Bacillus_subtilis
+spades.py -1 in506_5_R1.fastq.gz -2 in506_5_R2.fastq.gz -t 12 -m 512 -o in506_5_Escherichia_coli
+spades.py -1 in506_6_R1.fastq.gz -2 in506_6_R2.fastq.gz -t 12 -m 512 -o in506_6_Pseudomonas_aeruginosa
+spades.py -1 in506_7_R1.fastq.gz -2 in506_7_R2.fastq.gz -t 12 -m 512 -o in506_7_Listeria_monocytogenes
+spades.py -1 in506_8_R1.fastq.gz -2 in506_8_R2.fastq.gz -t 12 -m 512 -o in506_8_Saccharomyces_cerevisiae
+spades.py -1 in506_9_R1.fastq.gz -2 in506_9_R2.fastq.gz -t 12 -m 512 -o in506_9_Cryptococcus_neoformans
+spades.py -1 in506_10_R1.fastq.gz -2 in506_10_R2.fastq.gz -t 12 -m 512 -o in506_10_Lactobacillus_fermentum
