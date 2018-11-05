634642eb7d79f1886951fd7d75e8acb9428f73c1
Nick Loman <n.j.loman@bham.ac.uk>
2018-11-05 13:01:52 +0000

    kraken results on wtdbg2 assemblies

---
+++ analysis/wtdbg2/extractkrak.py
@@ -0,0 +1,14 @@
+import sys
+import csv
+
+print ("SampleID\tZymo\tPlatform\tExtraction\tSuperNatant\tLength\tLimit\tOverlaps\tContigID\tTaxon\tContigSize")
+for fn in sys.argv[1:]:
+    path, filename = fn.split("/")
+
+    conditions = path.split("-")
+
+    for ln in open(fn):
+        col = ln.split("\t")
+        taxa = col[2].split(" ")
+        print ("%s\t%s\t%s\t%s %s\t%s" % (path, "\t".join(conditions), col[1], taxa[0], taxa[1], col[3]))
+
